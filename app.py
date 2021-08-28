3c2be124e6eebea24680dc0265ec1bba8e066299
AshkanZare <ashkanzare77@gmail.com>
2021-08-28 19:57:51 +0430

    ready to deploy

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from Maktab_Group_Flask_Project.__init__ import create_app
-import os
 
-os.system('flask init-db')
 app = create_app()
 
 if __name__ == '__main__':
+    import os
+
+    os.system('flask init-db')
     app.run()
