174a40d518730f27e221dba2bf238c97414d1ac0
AshkanZare <ashkanzare77@gmail.com>
2021-08-28 20:17:44 +0430

    ready to deploy

--- instance/config.py
+++ instance/config.py
@@ -2,7 +2,7 @@ UPLOAD_PATH = 'media'
 SECRET_KEY = b'_5#y2L"F4Q8z\n\xec]/'
 MONGO_URI = 'mongodb+srv://niania77:niania77@flaskblogapp.phjgj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 MONGODB_SETTINGS = {
-    'db': 'flaskblogapp.phjgj.mongodb.net',
+    'db': 'flaskblogapp',
     'host': 'localhost',
     'port': 27017
 }
